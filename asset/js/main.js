bc71d8f6bda9d282c0916b8c2ba1f02b6c9ce4a0
Alex Lande <alexlande@gmail.com>
2012-10-17 11:52:26 -0700

    Clean up section anchors.

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,24 @@
-$('.information-nav a').click(function(e) {
-  var target = $(this).attr('href');
+$(function() {
 
-  $('.information-tab, .information-nav a').removeClass('active');
-  $(target).addClass('active');
-  $(this).addClass('active');
+  // information section tabs
 
-  e.preventDefault();
+  $('.information-nav a').click(function(e) {
+    var target = $(this).attr('href');
+
+    $('.information-tab, .information-nav a').removeClass('active');
+    $(target).addClass('active');
+    $(this).addClass('active');
+
+    e.preventDefault();
+  });
+
+  // add anchor links for all section headers
+
+  Array.prototype.slice.call(document.querySelectorAll('h2[id]')).forEach(function(h2) {
+    var a = document.createElement('a');
+    a.href = '#' + h2.id;
+    a.className = 'sct-anchor';
+    h2.appendChild(a);
+  });
+
 });
